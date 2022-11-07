bed9b5440cbef3ce1d76c3c20336fa605643c144
dkahsem27 <dkahsem27@gmail.com>
2022-11-07 15:18:54 +0900

    221107 초기환경세팅

---
+++ src/main/java/com/temlog/TemlogApplication.java
@@ -0,0 +1,13 @@
+package com.temlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temlog/TemlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
